19b5cec072b22d4f146693c9c3e9da676dfc0a73
atdilakshan <dilakshan0501@gmail.com>
2022-08-03 10:39:52 +0530

    authorization via role based(authvertify.js)

--- backend/helpers/authVerify.js
+++ backend/helpers/authVerify.js
@@ -1,11 +1,14 @@
+const { userModel } = require("../models/userSchema");
 const {tokenValidator} = require("./token");
 
 module.exports = async function(req, res, next) {
     try {
         const {jwt} = req.cookies;
         const valid = await tokenValidator(jwt);
-        const checkRole = await tokenValidator(jwt).role;
         if(valid) {
+            const phone_no = await tokenValidator(jwt).phone_no;
+            const findUser = await userModel.findOne({phone_no: phone_no});
+            const checkRole = await findUser.role;
             if(checkRole === "admin"){
                 next();
             } else {
