3eb27120ca5082a7b8e0cfb98d4ecf69ea92464b
Gowsi <gowsip2@gmail.com>
2022-08-04 06:54:11 +0530

    Image file upload

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cookieParser = require('cookie-parser');
 const dotenv = require("dotenv");
+const fileUpload = require("express-fileupload");
 
 const connectDB = require("./config/db"); // Database configuration
 
@@ -12,17 +12,24 @@ connectDB(); // Database Connection
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
 
 // Routes
 const authRoutes = require("./routes/authRoute");
 const userRoutes = require("./routes/userRoute");
 const productRoutes = require("./routes/productRoute");
+const upload = require("./routes/upload");
 
 const authVerify = require("./helpers/authVerify");
 
 app.use("/", authRoutes);
 app.use("/admin/users", authVerify, userRoutes);
 app.use("/admin/products", authVerify, productRoutes);
+app.use("/admin", authVerify, upload);
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
